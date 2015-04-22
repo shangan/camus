be0df7b1b2bf84e087538ee7e7b8426aa0d3ca51
zhangguanghui02 <zhangguanghui02@meituan.com>
2015-04-22 13:28:47 +0800

    add etl.starttime and etl.starttime.on

--- camus-meituan/src/main/java/com/meituan/camus/etl/SimpleStringMessageDecoder.java
+++ camus-meituan/src/main/java/com/meituan/camus/etl/SimpleStringMessageDecoder.java
@@ -17,6 +17,15 @@ public class SimpleStringMessageDecoder extends MeituanMessageDecoder {
 
   public void init(Properties props, String topicName) {
     super.init(props, topicName);
+    if (Boolean.parseBoolean(props.getProperty(Configuration.ETL_STARTTIME_ON, "false"))) {
+      if (props.getProperty(Configuration.ETL_STARTTIME) == null) {
+        throw new RuntimeException("configured etl.starttime.on, but no configure etl.starttime");
+      }
+      logger.info("etl starttime = " + props.getProperty(Configuration.ETL_STARTTIME));
+      beginTimeMillis = Long.valueOf(props.getProperty(Configuration.ETL_STARTTIME)) * 1000;
+      ignoreDeltaMillis = false;
+      DELTA_MILLIS = 3600000;
+    }
   }
 
   @Override
